c0c19fab5e0460d033c6615544a08d27dff06d6d
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-16 17:30:43 -0800

    pass test 2

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -15,10 +15,32 @@
 
         function convertToWords($numberInput)
         {
-            $digit1Map = array('0'=>'zero', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
+            $digitMap2 = array('0'=>' ', '1'=>'ten', '2'=>'twenty', '3'=>'thirty', '4'=>'forty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety');
 
-            $wordOutput = $digit1Map[$numberInput];
-            
+            $digitMap1 = array('0'=>'zero', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
+
+            // array created from user input number, split by digits
+            $numberInputSplit = str_split($numberInput);
+
+            $wordOutput = "";
+            $sizeofArray = sizeof($numberInputSplit);
+            var_dump($sizeofArray);
+            // for ($i = sizeof($numberInputSplit); $i > 0; $i--) {
+                if (sizeof($numberInputSplit) == 2 ) {
+                    $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap1[$numberInputSplit[1]];
+
+
+                } elseif (sizeof($numberInputSplit) == 1 ) {
+                    // var_dump($wordOutput);
+                    $wordOutput .= $digitMap1[$numberInputSplit[0]];
+                    // $wordOutput .= $digitMap1[$numberInput];
+                } else {
+                    return false;
+                }
+
+            // }
+
+
             return $wordOutput;
         }
     }
