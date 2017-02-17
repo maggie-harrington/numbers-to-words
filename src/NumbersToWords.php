b3ebd89af172591fbf1fa5dc13e9601848e2a4a2
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-16 16:19:32 -0800

    pass first test again, this time using associative array

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -15,7 +15,10 @@
 
         function convertToWords($numberInput)
         {
-            $wordOutput = 'two';
+            $digit1Map = array('0'=>'zero', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
+
+            $wordOutput = $digit1Map[$numberInput];
+            
             return $wordOutput;
         }
     }
