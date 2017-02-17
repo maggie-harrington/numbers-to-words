5c64b8f9e3d0cffcc1c0fff9d121e12366548e80
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-16 19:01:39 -0800

    pass test 9

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -15,6 +15,7 @@
 
         function convertToWords($numberInput)
         {
+
             $digitMap5 = array('0'=>'million', '1'=>'one million', '2'=>'two million', '3'=>'three million', '4'=>'four million', '5'=>'five million', '6'=>'six million', '7'=>'seven million', '8'=>'eight million', '9'=>'nine million');
 
             $digitMap4 = array('0'=>'thousand', '1'=>'one thousand', '2'=>'two thousand', '3'=>'three thousand', '4'=>'four thousand', '5'=>'five thousand', '6'=>'six thousand', '7'=>'seven thousand', '8'=>'eight thousand', '9'=>'nine thousand');
@@ -23,53 +24,68 @@
 
             $digitMap2 = array('0'=>'', '1'=>'ten', '2'=>'twenty', '3'=>'thirty', '4'=>'forty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety');
 
+            $digitMapTeen = array('0'=>'ten', '1'=>'eleven', '2'=>'twelve', '3'=>'thirteen', '4'=>'fourteen', '5'=>'fifteen', '6'=>'sixteen', '7'=>'seventeen', '8'=>'eighteen', '9'=>'nineteen');
+
             $digitMap1 = array('0'=>'', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
 
             // array created from user input number, split by digits
             $numberInputSplit = str_split($numberInput);
 
-
-
             $wordOutput = "";
             $sizeofArray = sizeof($numberInputSplit);
-            var_dump($sizeofArray);
-            // for ($i = sizeof($numberInputSplit); $i > 0; $i--) {
+
                 if ($sizeofArray == 1 ) {
-                    // var_dump($wordOutput);
-                    var_dump($digitMap1[$numberInputSplit[0]]);
                     if ($numberInputSplit[0] == '0') {
                         $wordOutput .= 'zero';
                     } else {
                         $wordOutput .= $digitMap1[$numberInputSplit[0]];
                     }
-                    // $wordOutput .= $digitMap1[$numberInput];
 
                 } elseif ($sizeofArray == 2 ) {
+                    if ($numberInputSplit[0] == '1') {
+                        $wordOutput = $wordOutput . $digitMapTeen[$numberInputSplit[1]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap1[$numberInputSplit[1]];
+                    }
 
-
-                    $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap1[$numberInputSplit[1]];
-                    
                 } elseif ($sizeofArray == 3 ) {
-                    $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap1[$numberInputSplit[2]];
+                    if ($numberInputSplit[1] == '1') {
+                        $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMapTeen[$numberInputSplit[2]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap1[$numberInputSplit[2]];
+                    }
 
                 } elseif ($sizeofArray == 4 ) {
-                    $wordOutput = $wordOutput . $digitMap4[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " . $digitMap2[$numberInputSplit[2]] . " " . $digitMap1[$numberInputSplit[3]];
+                    if ($numberInputSplit[2] == '1') {
+                        $wordOutput = $wordOutput . $digitMap4[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " .  $digitMapTeen[$numberInputSplit[3]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap4[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " . $digitMap2[$numberInputSplit[2]] . " " . $digitMap1[$numberInputSplit[3]];
+                    }
 
                 } elseif ($sizeofArray == 5 ) {
-                    $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap4[$numberInputSplit[1]] . " " . $digitMap3[$numberInputSplit[2]] . " " . $digitMap2[$numberInputSplit[3]] . " " . $digitMap1[$numberInputSplit[4]];
+                    if ($numberInputSplit[3] == '1') {
+                        $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap4[$numberInputSplit[1]] . " " . $digitMap3[$numberInputSplit[2]] . " " .  $digitMapTeen[$numberInputSplit[4]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap4[$numberInputSplit[1]] . " " . $digitMap3[$numberInputSplit[2]] . " " . $digitMap2[$numberInputSplit[3]] . " " . $digitMap1[$numberInputSplit[4]];
+                    }
 
                 } elseif ($sizeofArray == 6 ) {
-                    $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap4[$numberInputSplit[2]] . " " . $digitMap3[$numberInputSplit[3]] . " " . $digitMap2[$numberInputSplit[4]] . " " . $digitMap1[$numberInputSplit[5]];
+                    if ($numberInputSplit[4] == '1') {
+                        $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap4[$numberInputSplit[2]] . " " . $digitMap3[$numberInputSplit[3]] . " " .  $digitMapTeen[$numberInputSplit[5]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap4[$numberInputSplit[2]] . " " . $digitMap3[$numberInputSplit[3]] . " " . $digitMap2[$numberInputSplit[4]] . " " . $digitMap1[$numberInputSplit[5]];
+                    }
 
                 } elseif ($sizeofArray == 7 ) {
-                    $wordOutput = $wordOutput . $digitMap5[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " . $digitMap2[$numberInputSplit[2]] . " " . $digitMap4[$numberInputSplit[3]] . " " . $digitMap3[$numberInputSplit[4]] . " " . $digitMap2[$numberInputSplit[5]] . " " . $digitMap1[$numberInputSplit[6]];
+                    if ($numberInputSplit[5] == '1') {
+                        $wordOutput = $wordOutput . $digitMap5[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " . $digitMap2[$numberInputSplit[2]] . " " . $digitMap4[$numberInputSplit[3]] . " " . $digitMap3[$numberInputSplit[4]] . " " .  $digitMapTeen[$numberInputSplit[6]];
+                    } else {
+                        $wordOutput = $wordOutput . $digitMap5[$numberInputSplit[0]] . " " . $digitMap3[$numberInputSplit[1]] . " " . $digitMap2[$numberInputSplit[2]] . " " . $digitMap4[$numberInputSplit[3]] . " " . $digitMap3[$numberInputSplit[4]] . " " . $digitMap2[$numberInputSplit[5]] . " " . $digitMap1[$numberInputSplit[6]];
+                    }
 
                 } else {
                     return "Sorry, I can't count that high yet.";
                 }
-
-            // }
-
 
             return $wordOutput;
         }
