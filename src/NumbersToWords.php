89cd4c8b57f5f5a66ea639cc09af9e8770181b52
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-16 17:50:14 -0800

    pass test 3

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -15,9 +15,11 @@
 
         function convertToWords($numberInput)
         {
-            $digitMap2 = array('0'=>' ', '1'=>'ten', '2'=>'twenty', '3'=>'thirty', '4'=>'forty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety');
+            $digitMap3 = array('0'=>'', '1'=>'one hundred', '2'=>'two hundred', '3'=>'three hundred', '4'=>'four hundred', '5'=>'five hundred', '6'=>'six hundred', '7'=>'seven hundred', '8'=>'eight hundred', '9'=>'nine hundred');
 
-            $digitMap1 = array('0'=>'zero', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
+            $digitMap2 = array('0'=>'', '1'=>'ten', '2'=>'twenty', '3'=>'thirty', '4'=>'forty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety');
+
+            $digitMap1 = array('0'=>'', '1'=>'one', '2'=>'two', '3'=>'three', '4'=>'four', '5'=>'five', '6'=>'six', '7'=>'seven', '8'=>'eight', '9'=>'nine');
 
             // array created from user input number, split by digits
             $numberInputSplit = str_split($numberInput);
@@ -26,14 +28,19 @@
             $sizeofArray = sizeof($numberInputSplit);
             var_dump($sizeofArray);
             // for ($i = sizeof($numberInputSplit); $i > 0; $i--) {
-                if (sizeof($numberInputSplit) == 2 ) {
-                    $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap1[$numberInputSplit[1]];
-
-
-                } elseif (sizeof($numberInputSplit) == 1 ) {
+                if ($sizeofArray == 1 ) {
                     // var_dump($wordOutput);
                     $wordOutput .= $digitMap1[$numberInputSplit[0]];
                     // $wordOutput .= $digitMap1[$numberInput];
+
+                } elseif ($sizeofArray == 2 ) {
+
+                    // for ($i = 0; $i < $sizeofArray; $i++) {
+                    $wordOutput = $wordOutput . $digitMap2[$numberInputSplit[0]] . " " . $digitMap1[$numberInputSplit[1]];
+                    // }
+                } elseif ($sizeofArray == 3 ) {
+                    $wordOutput = $wordOutput . $digitMap3[$numberInputSplit[0]] . " " . $digitMap2[$numberInputSplit[1]] . " " . $digitMap1[$numberInputSplit[2]];
+                    
                 } else {
                     return false;
                 }
